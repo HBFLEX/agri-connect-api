82fc411d184a4b2a9d03aaa96e5261a7da9132a4
HBFLEX <happybanda@dyuni.ac.mw>
2023-11-03 00:36:44 +0200

    add community router

---
+++ routes/communityRouter.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const communityController = require('../controllers/communityController');
+
+
+router.get('/', communityController.getAllCommunities);
+router.get('/:id', communityController.getOneCommunity);
+router.post('/add', communityController.addCommunity);
+router.put('/update/:id', communityController.updateCommunity);
+router.delete('/delete/:id', communityController.deleteCommunity);
+
+
+module.exports = router;
